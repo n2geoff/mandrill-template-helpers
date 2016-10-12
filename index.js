fe7d921996357ed3edee4c5287d02e6cace1c74d
Geoff Doty <n2geoff@gmail.com>
2016-10-12 11:32:33 -0400

    added title, url, and striptags helpers

--- index.js
+++ index.js
@@ -2,22 +2,40 @@
 
 var helpers = {
     upper: function (value) {
-        return value.toUpperCase();
+        if (value && typeof value === 'string') {
+            return value.toUpperCase();
+        }
     },
     lower: function (value) {
-        return value.toLowerCase();
+        if (value && typeof value === 'string') {
+            return value.toLowerCase();
+        }
     },
     title: function (value) {
-        return value;
+        var words = [];
+        var response = [];
+
+        if (value && typeof value === 'string') {
+            //build words array
+            words = value.split(' ');
+
+            //capitalize each word
+            words.forEach(function (word) {
+                response.push(word.charAt(0).toUpperCase() + word.slice(1));
+            });
+
+            //return array of capitalized words
+            return response.join(' ');
+        }
     },
     url: function (value) {
-        return value;
+        return encodeURIComponent(value);
     },
     date: function (value) {
         return value;
     },
     striptags: function (value) {
-        return value;
+        return value.replace(/(<([^>]+)>)/ig, "");
     },
     unsub: function (value) {
         return value;
